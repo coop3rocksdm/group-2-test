d7fdab594497404a6e9d8abb2b3a8c15c88e59a3
coop3rock <coop3rocksdm@gmail.com>
2024-04-14 22:31:19 +0300

    Second commit

--- Group2/Group2/ViewController.swift
+++ Group2/Group2/ViewController.swift
@@ -11,10 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let rect = CGRect(x: 50, y: 50, width: 200, height: 200)
+        var simpleView = UIView(frame: rect)
+        simpleView.backgroundColor = .magenta
+        view.addSubview(simpleView)
     }
 
 //    My first changes
+    
+//    My second changes
 
 }
 
