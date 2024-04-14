9a7eb1c7e3516785d5552b0a5d3a300614a78fe7
coop3rock <coop3rocksdm@gmail.com>
2024-04-14 20:37:28 +0300

    Project restored

---
+++ Group2/Group2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group2
+//
+//  Created by Дмитро Солоп on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
